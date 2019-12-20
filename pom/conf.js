028fc377eb068aa2b424ab8e80cb5435835432f8
ortonikc <koushik350@gmail.com>
2019-12-20 16:30:53 +0530

    parallel execution with POM

--- pom/conf.js
+++ pom/conf.js
@@ -5,13 +5,21 @@ var HtmlReporter = require('protractor-beautiful-reporter');
 exports.config = {
   framework: 'jasmine',
   diretConnect: true,
-  specs: ['./testcases/tc001.js'],
+  specs: ['./testcases/tc001.js', './testcases/tc002.js'],
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 999999
   },
   params: {
-    baseUrl: 'http://leaftaps.com/opentaps/control/logout'
+    baseUrl: 'http://leaftaps.com/opentaps/control/logout',
+    userName: 'DemoSalesManager',
+    password: 'crmsfa'
+
+  },
+  capabilities: {
+    'browserName': 'chrome',
+    'shardTestFiles': true,
+    maxInstances: 2,
   },
   onPrepare: function () {
     // Add a screenshot reporter and store screenshots to `/tmp/screenshots`:
--- pom/testcases/tc002.js
+++ pom/testcases/tc002.js
@@ -3,18 +3,15 @@
 const st = require("./util.js")
 const init = new st.SetUp();
 const loginPage = require('../pages/loginPage.js')
-xdescribe('TC002', () => {
+describe('TC002', () => {
     beforeEach(() => {
         console.log("In Tc002");
         init.setUp();
     })
-    afterEach(()=>{
-        init.stop()
-    })
-    xit('Run for DemoCSR', () => {
+    it('Run for DemoCSR', () => {
         const login = new loginPage.LoginPage();
-        login.enterUserName('DemoCSR')
-        login.enterPassword('crmsfa')
+        login.enterUserName(browser.params.userName)
+        login.enterPassword(browser.params.password)
         let home = login.clickLoginBtn()
         home.verifyLoggedInName("Demo B2B CSR")
         home.clickCRMSFA()
