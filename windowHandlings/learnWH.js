e4e0debb5b7e082d58d0f06399ea46ea5badcae9
ortonikc <koushik350@gmail.com>
2019-12-21 18:19:33 +0530

    getWindowHandles

---
+++ windowHandlings/learnWH.js
@@ -0,0 +1,16 @@
+const driver = require('protractor')
+describe('Window Handling', () => {
+    it('Windows switching',async  () => {
+        driver.browser.ignoreSynchronization = true
+        driver.browser.get('http://leafground.com/pages/Window.html')
+        driver.element(driver.by.id('home')).click()
+       let windows = await driver.browser.getAllWindowHandles()
+       console.log(windows.length);
+       driver.browser.switchTo().window(windows[1])
+       driver.element(driver.by.xpath("(//a[@class='wp-categories-link maxheight'])[1]")).click()
+       console.log(await driver.browser.getTitle());
+       driver.browser.close();
+       driver.browser.switchTo().window(windows[0])
+       console.log(await driver.browser.getTitle());
+    })
+})
