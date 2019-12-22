b0b5dbba5ae62a537730a45359f10737f37ade1f
ortonikc <koushik350@gmail.com>
2019-12-22 17:30:35 +0530

    reduce

--- mapReduce/reduce.js
+++ mapReduce/reduce.js
@@ -5,18 +5,18 @@ describe('Learn Reduce', () => {
     it('Reduce', async () => {
         driver.browser.get('https://jqueryui.com/selectable/')
         driver.browser.switchTo().frame(0)
-        let lists = driver.element(driver.by.id('selectable')).$$('li')
-        console.log('count = ' + await lists.count());
-
-        let values = lists.reduce(async (acc, ele) => {
+        let items = driver.element(driver.by.id('selectable')).$$('li')
+        console.log('count = ' + await items.count());
+        // using async and await 
+        let values = items.reduce(async (acc, ele) => {
             return acc + await ele.getText() + ', '
-        }, 'init value')
-        // // console.log('expecting text');
-        let values = lists.reduce(function (acc, elem) {
-            return elem.getText().then(function (text) {
-                return acc + text + ' ';
-            });
-        }, '');
+        }, '')
+        // Using then function
+        // let values = lists.reduce(function (acc, elem) {
+        //     return elem.getText().then(function (text) {
+        //         return acc + text + ' ';
+        //     });
+        // }, '');
         console.log(await values);
 
     })
