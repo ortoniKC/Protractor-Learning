fe9654d402e8e0aecf0f8ffaa6fba50ec62220c4
ortonikc <koushik350@gmail.com>
2019-12-24 18:38:51 +0530

    removed credential

--- angularSites/qa.js
+++ angularSites/qa.js
@@ -2,8 +2,8 @@ const driver = require('protractor')
 describe('Check for angular', () => {
     it('Login', () => {
         driver.browser.get('https://qa.pr-cor3.arinspect.com/')
-        driver.element(driver.by.id('email')).sendKeys('prasanna@yopmail.com')
-        driver.element(driver.by.id('password')).sendKeys('Test@123')
+        driver.element(driver.by.id('email')).sendKeys('')
+        driver.element(driver.by.id('password')).sendKeys('')
         driver.element(driver.by.buttonText('Log In')).click()
     })
 })
