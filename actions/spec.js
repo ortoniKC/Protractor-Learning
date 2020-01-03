12b96904dae887ebb4ba52b1fe1e1530067cd755
ortoni <koushik350@gmail.com>
2020-01-03 11:34:27 +0530

    actions done

--- actions/spec.js
+++ actions/spec.js
@@ -1,21 +1,21 @@
 import { browser } from "protractor";
 
-// const  = require('protractor')
+const driver = require('protractor')
 describe("Learn Actions", () => {
   beforeEach(() => {
     browser.waitForAngularEnabled(false);
-    // browser.manage().window().maximize()
+    browser.manage().window().maximize()
     browser
       .manage()
       .timeouts()
       .implicitlyWait(20000);
   });
-  // xit('Drag & Drop', () => {
-  //     dragAndDrop()
-  // })
-  // xit('drag nad drop by', () => {
-  //     dragAndDropBy()
-  // })
+  it('Drag & Drop', () => {
+    dragAndDrop()
+  })
+  it('drag nad drop by', () => {
+    dragAndDropBy()
+  })
 
   it('selectable', () => {
     selectable()
@@ -30,21 +30,17 @@ async function sortable() {
   browser.get(url);
   browser.switchTo().frame(0);
   let it1 = element(by.xpath("//li[text()='Item 1']"));
-  // let it3 = element(by.xpath("//li[text()='Item 3']"))
+  // let it3 = element(by.xpath("//li[text( )='Item 3']"))
   let it5 = element(by.xpath("//li[text()='Item 5']"));
   var x, y;
   // console.log(it5.getLocation());
-  let loc = await it5.getLocation();
-  console.log("Locations: " + loc);
-
-  x = loc.x;
-  y = loc.y;
-  //   .then(loc => {
-  //   console.log("In then fn: " + loc.y + " " + loc.x);
-  //   x = loc.x;
-  //   y = loc.y;
-  // });
-  console.log(x + " " + y);
+  it5.getLocation()
+    .then(loc => {
+      console.log("In then fn: " + loc.y + " " + loc.x);
+      x = loc.x;
+      y = loc.y;
+      // console.log(x + " " + y);
+    });
   await browser
     .actions()
     .dragAndDrop(it1, { x: x, y: y })
@@ -67,21 +63,16 @@ function selectable() {
   browser.sleep(3000);
 }
 function dragAndDropBy() {
-  browser.get("https://jqueryui.com/draggable/");
-  browser.switchTo().frame(0);
-  let ele = element(by.id("draggable"))
-    .browser.actions()
-    .dragAndDrop(ele, { x: 150, y: 50 })
-    .perform();
-  browser.sleep(4000);
+  driver.browser.get("https://jqueryui.com/draggable/")
+  driver.browser.switchTo().frame(0);
+  let dra = driver.element(driver.by.id('draggable'))
+  driver.browser.actions().dragAndDrop(dra, { x: 100, y: 100 }).perform()
+  browser.sleep(3000)
 }
 function dragAndDrop() {
   browser.get("https://jqueryui.com/droppable/");
   browser.switchTo().frame(0);
   var drag = element(By.id("draggable"));
   var drop = element(By.id("droppable"));
-  browser
-    .actions()
-    .dragAndDrop(drag, drop)
-    .perform();
+  browser.actions().dragAndDrop(drag, drop).perform()
 }
